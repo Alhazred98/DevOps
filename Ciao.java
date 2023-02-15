10b9c0febfc9648fc890d7886a23f8ff8a80f015
Mario Sabatini <mariosabatini98@gmail.com>
2023-02-15 17:45:48 +0100

    delete

--- Ciao.java
+++
@@ -1,5 +0,0 @@
-public class Ciao{
-    public String sayHello(String name){
-        return "hello " + name;
-    }
-}
--- Example.java
+++
@@ -1,7 +0,0 @@
-public class Example {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        Ciao ciao = new Ciao();
-        ciao.sayHello("Stefania");
-    }
-}
